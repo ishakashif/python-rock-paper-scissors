ca425f3bfa5c268ad86296b7a0df59e49e23485a
Isha Kashif <ishak@udel.edu>
2025-08-07 09:24:51 -0400

    finished game for multiple rounds

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import random
 
 options = ("rock", "paper", "scissors")
-player = None
-computer = random.choice(options)
 
-while player not in options:
-    player = input("Enter a choice (rock, paper, scissors): ")
+playing = True
 
-print(f"Player: {player}")
-print(f"Computer: {computer}")
+while playing:
+    player = None
+    computer = random.choice(options)
+    while player not in options:
+        player = input("Enter a choice (rock, paper, scissors): ")
 
-if player == computer:
-    print("It's a tie!")
-elif player == "rock" and computer == "scissors":
-    print("You win!")
-elif player == "paper" and computer == "rock":
-    print("You win!")
-elif player == "scissors" and computer == "paper":
-    print("You win!")
-else:
-    print("You lose!")
+    print(f"Player: {player}")
+    print(f"Computer: {computer}")
+
+    if player == computer:
+        print("It's a tie!")
+    elif player == "rock" and computer == "scissors":
+        print("You win!")
+    elif player == "paper" and computer == "rock":
+        print("You win!")
+    elif player == "scissors" and computer == "paper":
+        print("You win!")
+    else:
+        print("You lose!")
+    play_again = input("Play again? (y/n): ").lower()
+    if not play_again == "y":
+        playing = False
+print("Thanks for playing!")
