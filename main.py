bef35055b42215201815241ca018a09e71a7c2e6
Isha Kashif <ishak@udel.edu>
2025-08-07 09:20:15 -0400

    finished game for 1 pass

--- main.py
+++ main.py
@@ -10,3 +10,13 @@ while player not in options:
 print(f"Player: {player}")
 print(f"Computer: {computer}")
 
+if player == computer:
+    print("It's a tie!")
+elif player == "rock" and computer == "scissors":
+    print("You win!")
+elif player == "paper" and computer == "rock":
+    print("You win!")
+elif player == "scissors" and computer == "paper":
+    print("You win!")
+else:
+    print("You lose!")
